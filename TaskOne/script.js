f4b20ca74a38cefacf7849a4ebeea513312545de
Myrzakan <alanfoureyes@gmail.com>
2024-03-01 23:32:14 +0600

    Front test job (JS)

--- TaskOne/script.js
+++ TaskOne/script.js
@@ -1,29 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const table = document.getElementById("currency-table")
+document.addEventListener('DOMContentLoaded', function () {
+  const table = document.getElementById('currency-table');
 
   fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1"
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1',
   )
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
       for (let i = 0; i < 5 && i < data.length; i++) {
         const currency = data[i];
-        console.log(currency)
-        const row = document.createElement("tr");
+        console.log(currency);
+        const row = document.createElement('tr');
         row.innerHTML = `
                   <td>${currency.id}</td>
                   <td>${currency.symbol}</td>
                   <td>${currency.name}</td>
               `;
 
-        if (currency.symbol === "usdt") {
-          row.classList.add("usdt");
+        if (currency.symbol === 'usdt') {
+          row.classList.add('usdt');
         } else if (i < 5) {
-          row.classList.add("blue-bg"); 
+          row.classList.add('blue-bg');
         }
 
         table.appendChild(row);
       }
     })
-    .catch((error) => console.error("Ошибка при получении данных:", error));
+    .catch(error => console.error('Ошибка при получении данных:', error));
 });
